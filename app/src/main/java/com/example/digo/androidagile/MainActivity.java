10ee716092bd336ad73a92bc868befeb66b4ff4a
Digo <romero.diego31@gmail.com>
2018-04-05 10:48:20 +0200

    Creation projet suite

--- app/src/main/java/com/example/digo/androidagile/MainActivity.java
+++ app/src/main/java/com/example/digo/androidagile/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.digo.androidagile;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Configuration config = getResources().getConfiguration();
+        onConfigurationChanged(config);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        }
     }
 }
